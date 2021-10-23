f6ea9eaec2d2cfde776bfd0e7dcfc7e85bd71467
Sahadat Hossen <181008112@eastdelta.edu.bd>
2021-10-24 03:42:23 +0600

    Added Polar NRZ L and Polar NRZ I

--- sketch.js
+++ sketch.js
@@ -1,46 +1,32 @@
 function setup() {
-  createCanvas(1300, 600);
+  createCanvas(1300, 1000);
 }
 var gap = 30;
-var b_number = "";
-var optn = "unipolar_NRZ";
+var b_number = "110101";
+var temp_b_number = b_number;
+var optn = "";
+var optn_text = "";
+var trnslt = 0;
 var color_value = '#ff0000';
-let strk_weight = 2.53*2;
-function draw() {
-  background(227);
-  translate(0, 100);
-  show_graph(5, 60);
-  stroke(color_value);
-  for (i = 0; i < b_number.length; i++) {
-    if (b_number[i] == 0) {
-      line(gap, 40 + 20, gap + gap * 2, 40 + 20);
+let strk_weight = 2.53;
+function draw(){
+  background(227); 
+  textSize(32);
+  textAlign(CENTER);
+  text(optn_text, width/2, 50);
+  translate(0,100);
+  //QPSK_temp();
+  
+    if(optn == "unipolar_NRZ"){
+      unipolar_NRZ();
     }
-    else if ((b_number[i] == 1)) {
-      line(gap, 40, gap + gap * 2, 40);
+    else if(optn == "polar_NRZ_L"){
+      polar_NRZ_L();
     }
-    translate(gap + gap, 0);
-    if ((b_number[i] != b_number[i + 1]) && i < b_number.length - 1) {
-      line(gap, 40, gap, 60);
+    else if(optn == "polar_NRZ_I"){
+      polar_NRZ_I();
     }
-    noStroke();
-    text(b_number[i], 0, 25);
-    stroke(color_value);
-  }
-  stroke(0, 0, 0);
-}
-
-function show_graph(x, y) {
-  strokeWeight(1);
-  line(x, y, x + width, y);
-  line(x, y + 40, x, y - 40);
-  for (i = -1; i * gap < width; i = i + 2) {
-    for (j = -10; j < 10; j++) {
-      line((i * gap), y + (5 * j), (i * gap), y + (j * 5) + 2);
+    else if(optn == "polar_RZ"){
+      polar_RZ();
     }
-  }
-  strokeWeight(strk_weight);
-}
-
-function get_number() {
-  b_number = document.getElementById('b_number').value;
 }
